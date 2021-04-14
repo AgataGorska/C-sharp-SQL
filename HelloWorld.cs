7428bbc48c4a3c4e61276e6ab0ed42a650b8fdfb
Agata Gorska <agata.gorska2109@gmail.com>
2021-04-14 11:06:09 +0100

    Changes made to HelloWorld and Numbers classes.

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -4,44 +4,79 @@ namespace PreworkBootcamp
 {
     class HelloWorld
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-			
-			string aFriend = "Bill";
-			Console.WriteLine(aFriend);
-			Console.WriteLine("Hello " + aFriend);
-			Console.WriteLine($"Hello {aFriend}");
+		static void Main(string[]args )
+		{
+			Console.WriteLine("Hello World!");
 
-			string firstFriend = "Maria";
-			string secondFriend = "Sage";
-			Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
-			Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
-			Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
-			string greeting = "      Hello World!       ";
-			Console.WriteLine($"[{greeting}]");
+			//string aFriend = "Bill";
+			//Console.WriteLine(aFriend);
+			//Console.WriteLine("Hello " + aFriend);
+			//Console.WriteLine($"Hello {aFriend}");
 
-			string trimmedGreeting = greeting.TrimStart();
-			Console.WriteLine($"[{trimmedGreeting}]");
+			//string firstFriend = "Maria";
+			//string secondFriend = "Sage";
+			//Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+			//Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+			//Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+			//string greeting = "      Hello World!       ";
+			//Console.WriteLine($"[{greeting}]");
 
-			trimmedGreeting = greeting.TrimEnd();
-			Console.WriteLine($"[{trimmedGreeting}]");
+			//string trimmedGreeting = greeting.TrimStart();
+			//Console.WriteLine($"[{trimmedGreeting}]");
 
-			trimmedGreeting = greeting.Trim();
-			Console.WriteLine($"[{trimmedGreeting}]");
-			string sayHello = "Hello World!";
-			Console.WriteLine(sayHello);
-			sayHello = sayHello.Replace("Hello", "Greetings");
-			Console.WriteLine(sayHello);
-			Console.WriteLine(sayHello.ToUpper());
-			Console.WriteLine(sayHello.ToLower());
+			//trimmedGreeting = greeting.TrimEnd();
+			//Console.WriteLine($"[{trimmedGreeting}]");
 
-			string songLyrics = "You say goodbye, and I say hello";
-			Console.WriteLine(songLyrics.StartsWith("You"));
-			Console.WriteLine(songLyrics.StartsWith("goodbye"));
+			//trimmedGreeting = greeting.Trim();
+			//Console.WriteLine($"[{trimmedGreeting}]");
+			//string sayHello = "Hello World!";
+			//Console.WriteLine(sayHello);
+			//sayHello = sayHello.Replace("Hello", "Greetings");
+			//Console.WriteLine(sayHello);
+			//Console.WriteLine(sayHello.ToUpper());
+			//Console.WriteLine(sayHello.ToLower());
 
-			Console.WriteLine(songLyrics.EndsWith("hello"));
-			Console.WriteLine(songLyrics.EndsWith("goodbye"));
+			//string songLyrics = "You say goodbye, and I say hello";
+			//Console.WriteLine(songLyrics.StartsWith("You"));
+			//Console.WriteLine(songLyrics.StartsWith("goodbye"));
+
+			//Console.WriteLine(songLyrics.EndsWith("hello"));
+			//Console.WriteLine(songLyrics.EndsWith("goodbye"));
+
+			// Integer
+			int a = 10;
+			int b = 200;
+			int c;
+			int max = int.MaxValue;
+			int min = int.MinValue;
+			Numbers n = new Numbers();
+			c = n.Integer(a, b);
+		Console.WriteLine("Integer returned: {0}",c);
+		Console.WriteLine("Integer min and max value: {0} {1}", min, max);
+
+			//Double
+			double d = 19;
+			double e = 76;
+			double f = 3;
+			double g;
+			Numbers n1 = new Numbers();
+			g = n1.Double(d,e,f);
+			Console.WriteLine("Double returned: {0}",g);
+			double max2 = double.MaxValue;
+			double min2 = double.MinValue;
+			Console.WriteLine("Double min and max value: {0} {1}", min2, max2);
+
+			//Decimal
+
+			decimal x = 1.0M;
+			decimal y = 7.0M;
+			decimal z;
+			decimal min3 = decimal.MinValue;
+			decimal max3 = decimal.MaxValue;
+			Numbers n2 = new Numbers();
+			z = n2.Decimal(x,y);
+			Console.WriteLine("Decimal returned: {0}", z);
+			Console.WriteLine("Decimal min and max value: {0} {1}", min3, max3);
 
 		}
     }
--- Numbers.cs
+++ Numbers.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace PreworkBootcamp
 {
-    class Numbers
+
+    public class Numbers
     {
+        public static Numbers number = new Numbers();
+        
+        //    int a = 5;
+
+
+        public int Integer(int a, int b)
+        {
+ 
+            int c = a + b;
+            return c;
+        }
+
+        public double Double(double d, double e, double f)
+        {
+
+            double g = (d+e)/f;
+            return g;
+        }
+
+        public decimal Decimal(decimal x, decimal y)
+        {
+            decimal z = x / y;
+            return z;
+        }
     }
 }

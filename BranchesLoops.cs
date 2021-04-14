552b4dc8ad350d3c09a1d2c0bf20cda0fa76e08c
Agata Gorska <agata.gorska2109@gmail.com>
2021-04-14 14:24:31 +0100

    More functionality added

--- BranchesLoops.cs
+++ BranchesLoops.cs
@@ -4,7 +4,58 @@ using System.Text;
 
 namespace PreworkBootcamp
 {
-    class BranchesLoops
+    public class BranchesLoops
     {
-    }
+		public static BranchesLoops branchesLoops = new BranchesLoops();
+
+		//if
+		public bool IFTest(int l, int m, int n)
+		{
+			bool test;
+			if ((l + m + n > 10) || (l == m))
+			{
+				test = true;
+			}
+			else
+			{
+				test = false;
+			}
+			return test;
+		}
+
+
+		//while
+		public int WhileTest()
+		{
+
+
+			int counter = 0;
+			while (counter < 10)
+			{
+				Console.WriteLine($"The counter is {counter}");
+				counter++;
+				Console.WriteLine();
+			}
+
+			return counter;
+		}
+
+		//task
+
+		public int Task()
+		{
+			int sum = 0;
+			for (int number = 1; number < 21; number++)
+			{
+				if (number % 3 == 0)
+				{
+					sum = sum + number;
+				}
+			}
+		
+			Console.WriteLine($"This is the sum of all integers 1 through 20 that are divisible by 3:  {sum}");
+			Console.WriteLine();
+			return sum;
+		}
+	}
 }
--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PreworkBootcamp
 {
@@ -7,6 +8,7 @@ namespace PreworkBootcamp
 		static void Main(string[]args )
 		{
 			Console.WriteLine("Hello World!");
+			Console.WriteLine();
 
 			//string aFriend = "Bill";
 			//Console.WriteLine(aFriend);
@@ -51,8 +53,9 @@ namespace PreworkBootcamp
 			int min = int.MinValue;
 			Numbers n = new Numbers();
 			c = n.Integer(a, b);
-		Console.WriteLine("Integer returned: {0}",c);
-		Console.WriteLine("Integer min and max value: {0} {1}", min, max);
+			Console.WriteLine("Integer returned: {0}",c);
+			Console.WriteLine("Integer min and max value: {0} {1}", min, max);
+			Console.WriteLine();
 
 			//Double
 			double d = 19;
@@ -65,6 +68,7 @@ namespace PreworkBootcamp
 			double max2 = double.MaxValue;
 			double min2 = double.MinValue;
 			Console.WriteLine("Double min and max value: {0} {1}", min2, max2);
+			Console.WriteLine();
 
 			//Decimal
 
@@ -77,7 +81,57 @@ namespace PreworkBootcamp
 			z = n2.Decimal(x,y);
 			Console.WriteLine("Decimal returned: {0}", z);
 			Console.WriteLine("Decimal min and max value: {0} {1}", min3, max3);
+			Console.WriteLine();
 
-		}
-    }
+
+			//Branches Loops
+			// if
+			bool test;
+			BranchesLoops n3 = new BranchesLoops();
+			test = n3.IFTest(3, 4, 5);
+			if (test == true)
+            {
+				Console.WriteLine("The answer is greater than 10");
+				Console.WriteLine("Or the first number is equal to the second");
+				Console.WriteLine();
+			}
+			else
+            {
+				Console.WriteLine("The answer is not greater than 10");
+				Console.WriteLine("And the first number is not equal to the second");
+				Console.WriteLine();
+			};
+
+			//while
+			//int counter;
+			BranchesLoops n4 = new BranchesLoops();
+			//counter =
+				n4.WhileTest();
+
+			//task
+			BranchesLoops n5 = new BranchesLoops();
+				n5.Task();
+
+			//ListCollections
+
+			var names = new List<string> { "Tomek", "Anna", "Jan" };
+			foreach (var name in names)
+			{
+				Console.WriteLine($"Hello {name.ToUpper()}!");
+			}
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Sorted names {name.ToUpper()}!");
+                Console.WriteLine();
+            }
+
+        }
+
+		//private static ListCollections ListCollections()
+		//{
+		//    throw new NotImplementedException();
+		//}
+	}
 }
